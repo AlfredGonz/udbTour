b941819451c989ec035fc71ec5e90c9a1ec7ce4d
ManuelMartinez96 <33555447+ManuelMartinez96@users.noreply.github.com>
2018-05-11 15:39:11 -0600

    Mapa SitioDeportes actualizado

--- app/src/main/java/com/example/alfredo/udbtour/MapsActivity.java
+++ app/src/main/java/com/example/alfredo/udbtour/MapsActivity.java
@@ -1,12 +1,19 @@
 package com.example.alfredo.udbtour;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -18,10 +25,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (status== ConnectionResult.SUCCESS){
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        }else{
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+            dialog.show();
+        }
     }
 
 
@@ -38,9 +55,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(13.702073, -89.224362);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
+        // Palacio Nacional de Los Deportes INDES
+        LatLng indes = new LatLng(13.703688, -89.195408);
+        mMap.addMarker(new MarkerOptions().position(indes).title("Palacio Nacional de Los Deportes INDES").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        //Estadio Nacional Jorge "El Magico" Gonzalez
+        LatLng estadio1 = new LatLng(13.698471, -89.215366);
+        mMap.addMarker(new MarkerOptions().position(estadio1).title("Estadio Nacional Jorge \"El Magico\" Gonzalez").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        //Circulo deportivo Internacional
+        LatLng circulo = new LatLng(13.695540, -89.227377);
+        mMap.addMarker(new MarkerOptions().position(circulo).title("Circulo deportivo Internacional").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        //Estadio Cuscatlan
+        LatLng estadio2 = new LatLng(13.681492, -89.223142);
+        mMap.addMarker(new MarkerOptions().position(estadio2).title("Estadio Cuscatlán").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        //Estadio de Futbol Playa Costa del Sol
+        LatLng playa = new LatLng(13.335652, -88.977421);
+        mMap.addMarker(new MarkerOptions().position(playa).title("Estadio Futbol Playa Costa del Sol").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        //Estadio Futbol Playa Apulo
+        LatLng apulo = new LatLng(13.702274, -89.077601);
+        mMap.addMarker(new MarkerOptions().position(apulo).title("Estadio Futbol Playa Apulo").icon(BitmapDescriptorFactory.fromResource(R.drawable.sitiosdepo)));
+
+        float zoomlevel=10;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(indes,zoomlevel));
     }
 }
--- app/src/main/java/com/example/alfredo/udbtour/SitiosDeportivos.java
+++ app/src/main/java/com/example/alfredo/udbtour/SitiosDeportivos.java
@@ -1,7 +1,10 @@
 package com.example.alfredo.udbtour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,29 +13,27 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SitiosDeportivos extends AppCompatActivity implements OnMapReadyCallback {
+public class SitiosDeportivos extends AppCompatActivity {
 
-    private GoogleMap mMap;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitios_deportivos);
 
+        button=(Button)findViewById(R.id.mapa);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync( this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(SitiosDeportivos.this,MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
-
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sv = new LatLng(13.702073, -89.224362);
-        mMap.addMarker(new MarkerOptions().position(sv).title("El Salvador"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sv));
-    }
 }
