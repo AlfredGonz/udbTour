457828c771373318f3d0f5e1953ed9dbe22ad08e
Ariel Chepito <arizul96@gmail.com>
2018-05-12 12:40:19 -0600

    bases del bsucador

--- app/src/main/java/com/example/alfredo/udbtour/allsites.java
+++ app/src/main/java/com/example/alfredo/udbtour/allsites.java
@@ -1,10 +1,13 @@
 package com.example.alfredo.udbtour;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -114,6 +117,52 @@ public class allsites  extends AppCompatActivity
         }
 
 
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+         /*   getMenuInflater().inflate(R.menu.menu_main, menu);
+
+            // Associate searchable configuration with the SearchView
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            searchView = (SearchView) menu.findItem(R.id.action_search)
+                    .getActionView();
+            searchView.setSearchableInfo(searchManager
+                    .getSearchableInfo(getComponentName()));
+            searchView.setMaxWidth(Integer.MAX_VALUE);
+
+            // listening to search query text change
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // filter recycler view when query submitted
+                    mAdapter.getFilter().filter(query);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String query) {
+                    // filter recycler view when text is changed
+                    mAdapter.getFilter().filter(query);
+                    return false;
+                }
+            });
+            return true;*/
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_search) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
 
 
 
